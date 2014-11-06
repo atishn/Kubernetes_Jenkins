742ac6bb0c323ccfb54f61fda1c87ec9aea207fc
Atish Narlawar <atish.narlawar@gmail.com>
2014-11-06 10:37:24 -0500

    Updated Deployment script

--- server.sh
+++ server.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # gcloud compute ssh --zone us-central1-b stormy
-docker pull huge/docker_stormy
-docker run -d -p 22 -p 5000:5000 -e "MASTERKUBEIP=130.211.113.209" --name stormy_app -t huge/docker_stormy
+#docker pull huge/docker_stormy
+#docker run -d -p 22 -p 5000:5000 -e "MASTERKUBEIP=130.211.113.209" --name stormy_app -t huge/docker_stormy
+
+gcloud compute ssh --zone us-central1-b stormy --command 'docker pull huge/docker_stormy && docker stop stormy_app && docker rm stormy_app && docker run -d -p 22 -p 5000:5000 -e "MASTERKUBEIP=130.211.113.209" --name stormy_app -t huge/docker_stormy'
 
